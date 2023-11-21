f1e45c77ff2bc7d3cd212e81a0340b1529e74e60
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-21 19:39:33 +0530

    adding new contact to address book

---
+++ src/bdlabzz3/Add_contact.java
@@ -0,0 +1,42 @@
+package bdlabzz3;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Add_contact {
+	Contact_details contact = new Contact_details();
+	ArrayList<Contact_details> list = new ArrayList<Contact_details>(); 
+	void display_Data() {
+		for(Contact_details contacts : list) {
+			System.out.println(contacts);
+		}
+		
+	}
+	void add_Contact(){
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the number of records that need to be added in address book:" );
+		int n=scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println("enter the first name:");
+			contact.setFirst_name(scan.next());
+			System.out.println("enter the last name:");
+			contact.setLast_name(scan.next());
+			System.out.println("enter the address:");
+			contact.setAddress(scan.next());
+			System.out.println("enter the city:");
+			contact.setCity(scan.next());
+			System.out.println("enter the state:");
+			contact.setState(scan.next());
+			System.out.println("enter the zip code of the residency:");
+			contact.setZip(scan.next());
+			System.out.println("enter the phone number:");
+			contact.setPhone_no(scan.next());
+			System.out.println("enter the email_id:");
+			contact.setEmail_id(scan.next());
+			list.add(contact);
+	      }
+	}
+
+}
---
+++ src/bdlabzz3/Contact_details.java
@@ -0,0 +1,86 @@
+package bdlabzz3;
+
+public class Contact_details {
+	private String first_name;
+	private String last_name;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone_no;
+    private String email_id;
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhone_no() {
+		return phone_no;
+	}
+
+	public void setPhone_no(String phone_no) {
+		this.phone_no = phone_no;
+	}
+
+	public String getEmail_id() {
+		return email_id;
+	}
+
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact_details [first_name=" + first_name + ", last_name=" + last_name + ", address=" + address
+				+ ", city=" + city + ", state=" + state + ", zip=" + zip + ", phone_no=" + phone_no + ", email_id="
+				+ email_id + "]";
+	}
+	
+	
+
+}
